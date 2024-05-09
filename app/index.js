5ce30deb47fe1644d895e0fb5db6c3565c8ab7f5
UnMi <unmikorin@gmail.com>
2024-05-09 22:38:18 +0300

    Initial

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { View, Text} from 'react-native';
+import 'dart:io';
+import 'dart:ui';
+import 'package:camera/camera.dart';
+import 'package:first/screen/camera_screen.dart';
+import 'package:flutter/material.dart';
+
+
+const Home = () => {
+
+    return (
+        <View>
+        <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
